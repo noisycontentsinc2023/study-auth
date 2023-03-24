c3e54fc11d3169dd3e2f6ef6cc973104f7d4411a
piaotaijiong <127836167+piaotaijiong@users.noreply.github.com>
2023-03-24 14:57:19 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -19,24 +19,28 @@ prefix = '!'
 bot = commands.Bot(command_prefix=prefix, intents=intents)
 
 @bot.command(name='고정')
-async def pin(ctx, message_id: int):
+async def pin(ctx, message_id: str):
     try:
         channel = ctx.channel
-        message = await channel.fetch_message(message_id)
+        message = await channel.fetch_message(int(message_id))
         await message.pin()
         await ctx.send(f"Message {message_id} pinned to the bottom of the channel.")
+    except ValueError:
+        await ctx.send(f"Invalid input: {message_id} is not a valid integer.")
     except discord.NotFound:
-        await ctx.send("Message not found.")
+        await ctx.send(f"Message {message_id} not found.")
 
 @bot.command(name='해제')
-async def unpin(ctx, message_id: int):
+async def unpin(ctx, message_id: str):
     try:
         channel = ctx.channel
-        message = await channel.fetch_message(message_id)
+        message = await channel.fetch_message(int(message_id))
         await message.unpin()
         await ctx.send(f"Message {message_id} unpinned.")
+    except ValueError:
+        await ctx.send(f"Invalid input: {message_id} is not a valid integer.")
     except discord.NotFound:
-        await ctx.send("Message not found.")
+        await ctx.send(f"Message {message_id} not found.")
     
 #Run the bot
 bot.run(TOKEN)
