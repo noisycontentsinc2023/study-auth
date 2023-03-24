14a9ec4254f124fb1fa288e39b7b2ac11a631720
piaotaijiong <127836167+piaotaijiong@users.noreply.github.com>
2023-03-24 12:00:25 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ import bs4
 import asyncio
 import os
 import urllib
+import requsests
 from discord import Embed
 from discord.ext import tasks, commands
 from discord.utils import get
@@ -11,6 +12,11 @@ from urllib.request import Request
 naver_client_id = 'iuWr9aAAyKxNnRsRSQIt'
 naver_client_secret = 'bkfPugeyIa'
 
-
 TOKEN = os.environ['TOKEN']
 PREFIX = os.environ['PREFIX']
+
+intents = discord.Intents.default()
+intents.members = True
+intents=discord.Intents.all()
+prefix = '!'
+bot = commands.Bot(command_prefix=prefix, intents=intents)
