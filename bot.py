06348a822c85470fdc5a3e4bfdf41b01e8b226a8
piaotaijiong <127836167+piaotaijiong@users.noreply.github.com>
2023-03-27 17:12:56 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -23,31 +23,25 @@ intents.presences = False
 bot = commands.Bot(command_prefix='!', intents=intents)
 openai.api_key = "OPENAI"
 
-async def process_gpt_request(prompt):
+async def generate_response(prompt):
     response = openai.Completion.create(
         engine="text-davinci-002",
         prompt=prompt,
-        temperature=0.7,
-        max_tokens=100,
-        top_p=1,
-        frequency_penalty=0,
-        presence_penalty=0,
+        max_tokens=150,
+        n=1,
+        stop=None,
+        temperature=0.5,
     )
-    return response.choices[0].text.strip()
 
-def is_topic_allowed(question):
-    allowed_topics = ["language", "korean", "chinese", "japanese", "spanish", "german", "french"]
-    return any(topic in question for topic in allowed_topics)
+    message = response.choices[0].text.strip()
+    tokens_used = response.choices[0].usage["total_tokens"]
+    return message, tokens_used
 
-@bot.command(name='gpt')
-async def gpt(ctx, *, question):
-    question = question.strip().lower()
+@bot.command(name="gpt")
+async def gpt(ctx, *, message):
+    prompt = f"{ctx.author.name}: {message}"
+    response, tokens_used = await generate_response(prompt)
+    await ctx.send(f"Response: {response}\nTokens used: {tokens_used}")
 
-    if is_topic_allowed(question):
-        response = await process_gpt_request(question)
-        await ctx.send(response)
-    else:
-        await ctx.send("I can only answer language-related questions or questions related to Korean, Chinese, Japanese, Spanish, German, or French. Please try asking a question related to one of these topics.")
-    
 #Run the bot
 bot.run(TOKEN)
