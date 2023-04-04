3366684a0d5599254d14748c10e62fb482697d2d
piaotaijiong <127836167+piaotaijiong@users.noreply.github.com>
2023-04-04 16:46:33 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -240,9 +240,10 @@ async def delete_memo(ctx, memo_number: int):
 
     # Add the following lines to update memo numbers for the remaining memos
     for i in range(index_to_delete - 1, len(memo_values) - 1):
-        current_memo = memo_values[i + 1]
-        updated_memo = f"{user_id}: {current_memo.split(': ')[1]}"
-        sheet.update_cell(i + 2, col, updated_memo)
+        current_memo = memo_values[i + 1] if i + 1 < len(memo_values) else ''
+        if current_memo:
+            updated_memo = f"{user_id}: {current_memo.split(': ')[1]}"
+            sheet.update_cell(i + 2, col, updated_memo)
 
     await ctx.send(f'{ctx.author.mention} memo {memo_number} deleted.')
         
