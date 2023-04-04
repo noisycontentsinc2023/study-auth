1256423bf0dc3541857b07ab50f18a7b24227875
piaotaijiong <127836167+piaotaijiong@users.noreply.github.com>
2023-04-04 11:13:51 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -133,12 +133,10 @@ async def uncheck(ctx, option_num: int):
         
 #-------------------------메-------------------------#
 
-memo_dict = {}
 sheet_name = '메모'
 spreadsheet_url = 'https://docs.google.com/spreadsheets/d/19fztMa5sCm_yE5cyYoKpbF015QFq2xFThyiEYjkNi6w/edit?usp=sharing'
 
 def load_memo():
-    global memo_dict
     try:
         scopes = ['https://www.googleapis.com/auth/drive']
         credentials = service_account.Credentials.from_service_account_info({
@@ -162,11 +160,14 @@ def load_memo():
             else:
                 memo_dict[row[0]] = [row[1]]
         print("Loaded memo from Google Spreadsheet")
+        return memo_dict
     except Exception as e:
+        print(f"Error while loading memo from Google Spreadsheet: {e}")
+        return {}
         print(f"Error while loading memo from Google Spreadsheet: {e}")
         memo_dict = {}
 
-def save_memo():
+def save_memo(memo_dict):
     try:
         scopes = ['https://www.googleapis.com/auth/drive']
         credentials = service_account.Credentials.from_service_account_info({
@@ -199,13 +200,27 @@ async def memo(ctx, *, memo):
     user = str(ctx.author)
 
     # Open the Google Sheet
-    sheet = client.open("Memo").sheet1
+    scopes = ['https://www.googleapis.com/auth/drive']
+    credentials = service_account.Credentials.from_service_account_info({
+        'type': 'service_account',
+        'project_id': 'thematic-bounty-382700',
+        'private_key_id': '502d8dd4f035d15b57bff64ee323d544d28aedc4',
+        'private_key': 'MIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQD4Kze3Hn/yxevG\nzHUklYGSDDs8qKQeyYdd1eWaR0PNKZ2+nwKFGmXGENS6vuy3U81dqI3AVgA3w6UW\nHEaVfPvc31OX5yNCIS0eQxxqWWGJJ5+MbvUC06qXi/7hCup0WK+hoqwjHtUX7AYu\nEDgtf6xd29gSvb3YXs6pvi+2tpwPt0SED6HGPU7qPRsAaPnyUsVCj/mW04ca2iea\nxMsIqxKT6ufNssiXX7qhKyziowneM0lp8BB3k2z+/FGPQOCdi/lIscC9zKbDOIcb\nOZw+B2sd2opp7Dwo3JMIkh3NJevw9hjp0+CFeqemGNsCAiSuFkvydx6BagWaWAPs\nC48nZLNZAgMBAAECggEAF3uUbTMZQZZVoAU5CPYOMY0PfmcJR6IDeX8715BKg8N+\nOhtHBGQJ8Rbm4Ehgcxz+i/AfAK4KnXw5dvkEO1E9Lmph+Tfdg9yKjchlLBGK24z4\nqZPWwpaXl/k+7BnJs2pwbROs5PJeEOJMN+fgPvrrqyJ6RNS4Pf0Dond9AZWwOQLL\naJPFZryK7Bmvtt0H8mDDWdfqmCQTtPJ9PUyDEUeenlyhuek8wH3GHcghOSlsCDF1\nW/3YXM9Vr/arE4V6hTLwXofrUnTuXTfo+DcaOIXpHqIPS+nCyzWZ0kAJ7/uKjhnN\nF4kgv9aXDX9Y7S+3irXazRhowfu2rGuPRO/2+FCuMQKBgQD+JRDctOrKvpl9zDaw\nWT2a3qmYuFf90+b8EkKsWzVBM7neEJlw1ZWxUZzkdHXwkxcM7w93BjZeXJRnI7HZ\n5sHMrRw3p7Cwy0REqC3GSbYMCIZ/98y5Ot5sOXamUCOtYnic1NG2PBzr9h94Nt7d\nZu9D7cD/kaogm9Fl9t1VMD3REQKBgQD5+vvxY0nUkzrPEHfAOnPRqt3fM9ryzmk9\n8WyffmWqaDcvb9pl1F/+/51u00cvh2Q6etvL0J850AB0AKC9QdYzIaSj4LeRNzjA\ns+K6Po5+HAYezxC1cYzFh+slAfX3gX9pa11f3aOltj4h7IXvqBB0iH4rl/i2KQ/G\ntSDa62K9yQKBgAXXEDYiKisSijBr2u3efx3p8/fAdLUug2ZTfRi819Jxv9msg/ol\nzlTOzU4qpvMqTiNL8w0HJYSxl+9u0I1zUgzEBZv5zIOjiCQTwUmHNBm+sGiMZzXy\ndl4CTAmyWb+IPcFM2qzXYMrDUyHOEP0BeooTEpZM4J3zNrKjI57rhuAhAoGAKWDC\nE1K8BdPZCC1RpSAHy8zcrPWIaGiCQx6TPFNPwMU/XTrGi9R7j1oAVTfjsJpYnNV5\nTGNb99XWPV1dPfaH3i7TcczglcjuO/eKsAlqzLUWzkK4IVCKXKgC5D1O2Yk17d03\nt4aYb/Wak0LzaJgJIUD2oYCmSoDBe8K/jX0o+wECgYBnxk9HR/23hjWaxrSnXGDB\nHxLXg9Wz5w0N+gdC/FNxknFOft+nsCMKWMocOtGYhJU3OvkTYYqL1iDsKoMb74xG\nVwB1fuoNrNp+aJ/CzbtZVT1WLzXG41e9cu2TuOy+wpDlryfJAZ6KNVgDOmhh8TR2\nz7T0rt1QSfOZILpiwpR4jg==',
+        'client_email': 'noisycontents@thematic-bounty-382700.iam.gserviceaccount.com',
+        'client_id': '107322055541690533468',
+        'auth_uri': 'https://accounts.google.com/o/oauth2/auth',
+        'token_uri': 'https://accounts.google.com/o/oauth2/token',
+        'auth_provider_x509_cert_url': 'https://www.googleapis.com/oauth2/v1/certs',
+        'client_x509_cert_url': 'https://www.googleapis.com/robot/v1/metadata/x509/noisycontents%40thematic-bounty-382700.iam.gserviceaccount.com'
+        }, scopes=scopes)
+    gc = gspread.authorize(credentials)
+    sh = gc.open_by_url(spreadsheet_url).worksheet(sheet_name)
 
     # Append the user's name and memo to the sheet
-    sheet.append_row([user, memo])
+    sh.append_row([user, memo])
 
     # Send a confirmation message
-    await ctx.send(f"{user}, your memo has been saved.")
+    await ctx.send(f"{user}, 메모가 정상적으로 저장됐습니")
 
 # Define the view command
 @bot.command(name='메모보기')
@@ -214,7 +229,7 @@ async def view(ctx):
     user = str(ctx.author)
 
     # Open the Google Sheet
-    sheet = client.open("Memo").sheet1
+    sheet = bot.open("Memo").sheet1
 
     # Search for the user's name in the sheet
     cell = sheet.find(user)
