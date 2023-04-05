98ca99769bf71f0912a65d7a80adcc60f59d34dc
piaotaijiong <127836167+piaotaijiong@users.noreply.github.com>
2023-04-05 14:00:17 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -54,46 +54,81 @@ async def gpt(ctx, *, message):
     await ctx.send(embed=embed)
     
 #------------------------------------------------#
+scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+creds_info = {
+  "type": "service_account",
+  "project_id": "thematic-bounty-382700",
+  "private_key_id": "502d8dd4f035d15b57bff64ee323d544d28aedc4",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQD4Kze3Hn/yxevG\nzHUklYGSDDs8qKQeyYdd1eWaR0PNKZ2+nwKFGmXGENS6vuy3U81dqI3AVgA3w6UW\nHEaVfPvc31OX5yNCIS0eQxxqWWGJJ5+MbvUC06qXi/7hCup0WK+hoqwjHtUX7AYu\nEDgtf6xd29gSvb3YXs6pvi+2tpwPt0SED6HGPU7qPRsAaPnyUsVCj/mW04ca2iea\nxMsIqxKT6ufNssiXX7qhKyziowneM0lp8BB3k2z+/FGPQOCdi/lIscC9zKbDOIcb\nOZw+B2sd2opp7Dwo3JMIkh3NJevw9hjp0+CFeqemGNsCAiSuFkvydx6BagWaWAPs\nC48nZLNZAgMBAAECggEAF3uUbTMZQZZVoAU5CPYOMY0PfmcJR6IDeX8715BKg8N+\nOhtHBGQJ8Rbm4Ehgcxz+i/AfAK4KnXw5dvkEO1E9Lmph+Tfdg9yKjchlLBGK24z4\nqZPWwpaXl/k+7BnJs2pwbROs5PJeEOJMN+fgPvrrqyJ6RNS4Pf0Dond9AZWwOQLL\naJPFZryK7Bmvtt0H8mDDWdfqmCQTtPJ9PUyDEUeenlyhuek8wH3GHcghOSlsCDF1\nW/3YXM9Vr/arE4V6hTLwXofrUnTuXTfo+DcaOIXpHqIPS+nCyzWZ0kAJ7/uKjhnN\nF4kgv9aXDX9Y7S+3irXazRhowfu2rGuPRO/2+FCuMQKBgQD+JRDctOrKvpl9zDaw\nWT2a3qmYuFf90+b8EkKsWzVBM7neEJlw1ZWxUZzkdHXwkxcM7w93BjZeXJRnI7HZ\n5sHMrRw3p7Cwy0REqC3GSbYMCIZ/98y5Ot5sOXamUCOtYnic1NG2PBzr9h94Nt7d\nZu9D7cD/kaogm9Fl9t1VMD3REQKBgQD5+vvxY0nUkzrPEHfAOnPRqt3fM9ryzmk9\n8WyffmWqaDcvb9pl1F/+/51u00cvh2Q6etvL0J850AB0AKC9QdYzIaSj4LeRNzjA\ns+K6Po5+HAYezxC1cYzFh+slAfX3gX9pa11f3aOltj4h7IXvqBB0iH4rl/i2KQ/G\ntSDa62K9yQKBgAXXEDYiKisSijBr2u3efx3p8/fAdLUug2ZTfRi819Jxv9msg/ol\nzlTOzU4qpvMqTiNL8w0HJYSxl+9u0I1zUgzEBZv5zIOjiCQTwUmHNBm+sGiMZzXy\ndl4CTAmyWb+IPcFM2qzXYMrDUyHOEP0BeooTEpZM4J3zNrKjI57rhuAhAoGAKWDC\nE1K8BdPZCC1RpSAHy8zcrPWIaGiCQx6TPFNPwMU/XTrGi9R7j1oAVTfjsJpYnNV5\nTGNb99XWPV1dPfaH3i7TcczglcjuO/eKsAlqzLUWzkK4IVCKXKgC5D1O2Yk17d03\nt4aYb/Wak0LzaJgJIUD2oYCmSoDBe8K/jX0o+wECgYBnxk9HR/23hjWaxrSnXGDB\nHxLXg9Wz5w0N+gdC/FNxknFOft+nsCMKWMocOtGYhJU3OvkTYYqL1iDsKoMb74xG\nVwB1fuoNrNp+aJ/CzbtZVT1WLzXG41e9cu2TuOy+wpDlryfJAZ6KNVgDOmhh8TR2\nz7T0rt1QSfOZILpiwpR4jg==\n-----END PRIVATE KEY-----\n",
+  "client_email": "noisycontents@thematic-bounty-382700.iam.gserviceaccount.com",
+  "client_id": "107322055541690533468",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/noisycontents%40thematic-bounty-382700.iam.gserviceaccount.com"
+}
+creds = service_account.Credentials.from_service_account_info(info=creds_info, scopes=scope)
+client = gspread.authorize(creds)
+sheet = client.open('테스트').worksheet('')
 
-@bot.command(name='가위')
-async def rock_paper_scissors(ctx):
-    user_choice = '가위'
-    await play_game(user_choice, ctx)
+# 저장된 고정 메시지를 불러옵니다.
+def load_sticky_messages():
+    global sticky_messages
+    try:
+        data = sheet.get_all_values()
+        sticky_messages = {int(row[0]): row[1] for row in data}
+    except gspread.exceptions.APIError:
+        sticky_messages = {}
 
-@bot.command(name='바위')
-async def rock_paper_scissors(ctx):
-    user_choice = '바위'
-    await play_game(user_choice, ctx)
 
-@bot.command(name='보')
-async def rock_paper_scissors(ctx):
-    user_choice = '보'
-    await play_game(user_choice, ctx)
+# 고정 메시지를 스프레드시트에 저장합니다.
+def save_sticky_messages():
+    global sticky_messages
+    sheet.clear()
+    for channel_id, message in sticky_messages.items():
+        sheet.append_row([channel_id, message])
 
-async def play_game(user_choice, ctx):
-    rps = ['가위', '바위', '보']
-    bot_choice = random.choice(rps)
 
-    result = None
-    if user_choice == bot_choice:
-        result = '비겼습니다!'
-        color = discord.Color.dark_gray()
-        emoji = '🤝'
-    elif (user_choice == '가위' and bot_choice == '보') or \
-         (user_choice == '바위' and bot_choice == '가위') or \
-         (user_choice == '보' and bot_choice == '바위'):
-        result = '테이망령이 졌습니다!😭'
-        color = discord.Color.green()
-        emoji = '🎉'
+# 봇 시작시 스프레드시트에서 고정 메시지를 불러옵니다.
+@bot.event
+async def on_ready():
+    load_sticky_messages()
+    print('봇이 성공적으로 시작되었습니다.')
+
+
+# 고정 메시지를 추가하고, 스프레드시트에 저장합니다.
+@bot.command(name='고정')
+@has_specific_roles(allowed_role_ids)
+async def sticky(ctx, *, message):
+    global sticky_messages
+    sticky_messages[ctx.channel.id] = message
+    await ctx.send(f'메시지가 고정됐습니다!')
+    save_sticky_messages()
+
+
+# 고정 메시지를 삭제하고, 스프레드시트에 저장합니다.
+@bot.command(name='해제')
+@has_specific_roles(allowed_role_ids)
+async def unsticky(ctx):
+    global sticky_messages
+    if ctx.channel.id in sticky_messages:
+        del sticky_messages[ctx.channel.id]
+        await ctx.send('고정이 해제됐어요!')
+        save_sticky_messages()
     else:
-        result = '테이망령이 이겼습니다!🥳'
-        color = discord.Color.red()
-        emoji = '😭'
+        await ctx.send('이 채널에는 고정된 메시지가 없어요')
 
-    embed = discord.Embed(title=f'{user_choice} 대 {bot_choice}', description=result, color=color)
-    embed.set_author(name='게임 결과')
 
-    await ctx.send(embed=embed)
+# 스프레드시트에서 불러온 고정 메시지를 사용하여 메시지를 고정합니다.
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+
+    await bot.process_commands(message)
+
+    global sticky_messages
+    global last_sticky_messages
     
 #Run the bot
 bot.run(TOKEN)
