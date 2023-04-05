0119b511dfa6211538c91ee5e79475eaed263cbf
piaotaijiong <127836167+piaotaijiong@users.noreply.github.com>
2023-04-05 12:02:04 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -344,6 +344,13 @@ class MenuSelector(discord.ui.View):
 
         await interaction.response.edit_message(embed=food, view=self)
         
+    async def recommend_callback(self, interaction: discord.Interaction):
+        selected_food = random.choice(self.foods[self.category])
+        food = discord.Embed(title=f"{self.category} 추천메뉴", description="아래 추천받기 버튼을 클릭해서 메뉴를 추천받아보세요!", color=0x00ff00)
+        food.add_field(name="메뉴", value=f"{selected_food}")
+        food.set_footer(text="맛있게 드세요! 🥳")
+        food.set_author(name="스터디미니 음식점 추천맵", url="https://www.google.com/maps/d/edit?mid=1-le8EVMGB6tH-4ryziNUUub1XyOSgHI&usp=sharing")
+                        
 @bot.command(name='메뉴추천')
 async def menu_recommendation(ctx):
     selector_view = MenuSelector()
