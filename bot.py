1ad688149bd618ae5c5526183b82d6e6df157b1a
piaotaijiong <127836167+piaotaijiong@users.noreply.github.com>
2023-04-06 17:35:26 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -33,6 +33,22 @@ intents.presences = False
 bot = commands.Bot(command_prefix=PREFIX, intents=intents)
 openai.api_key = OPENAI
 
+scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+creds_info = {
+  "type": "service_account",
+  "project_id": "thematic-bounty-382700",
+  "private_key_id": "502d8dd4f035d15b57bff64ee323d544d28aedc4",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQD4Kze3Hn/yxevG\nzHUklYGSDDs8qKQeyYdd1eWaR0PNKZ2+nwKFGmXGENS6vuy3U81dqI3AVgA3w6UW\nHEaVfPvc31OX5yNCIS0eQxxqWWGJJ5+MbvUC06qXi/7hCup0WK+hoqwjHtUX7AYu\nEDgtf6xd29gSvb3YXs6pvi+2tpwPt0SED6HGPU7qPRsAaPnyUsVCj/mW04ca2iea\nxMsIqxKT6ufNssiXX7qhKyziowneM0lp8BB3k2z+/FGPQOCdi/lIscC9zKbDOIcb\nOZw+B2sd2opp7Dwo3JMIkh3NJevw9hjp0+CFeqemGNsCAiSuFkvydx6BagWaWAPs\nC48nZLNZAgMBAAECggEAF3uUbTMZQZZVoAU5CPYOMY0PfmcJR6IDeX8715BKg8N+\nOhtHBGQJ8Rbm4Ehgcxz+i/AfAK4KnXw5dvkEO1E9Lmph+Tfdg9yKjchlLBGK24z4\nqZPWwpaXl/k+7BnJs2pwbROs5PJeEOJMN+fgPvrrqyJ6RNS4Pf0Dond9AZWwOQLL\naJPFZryK7Bmvtt0H8mDDWdfqmCQTtPJ9PUyDEUeenlyhuek8wH3GHcghOSlsCDF1\nW/3YXM9Vr/arE4V6hTLwXofrUnTuXTfo+DcaOIXpHqIPS+nCyzWZ0kAJ7/uKjhnN\nF4kgv9aXDX9Y7S+3irXazRhowfu2rGuPRO/2+FCuMQKBgQD+JRDctOrKvpl9zDaw\nWT2a3qmYuFf90+b8EkKsWzVBM7neEJlw1ZWxUZzkdHXwkxcM7w93BjZeXJRnI7HZ\n5sHMrRw3p7Cwy0REqC3GSbYMCIZ/98y5Ot5sOXamUCOtYnic1NG2PBzr9h94Nt7d\nZu9D7cD/kaogm9Fl9t1VMD3REQKBgQD5+vvxY0nUkzrPEHfAOnPRqt3fM9ryzmk9\n8WyffmWqaDcvb9pl1F/+/51u00cvh2Q6etvL0J850AB0AKC9QdYzIaSj4LeRNzjA\ns+K6Po5+HAYezxC1cYzFh+slAfX3gX9pa11f3aOltj4h7IXvqBB0iH4rl/i2KQ/G\ntSDa62K9yQKBgAXXEDYiKisSijBr2u3efx3p8/fAdLUug2ZTfRi819Jxv9msg/ol\nzlTOzU4qpvMqTiNL8w0HJYSxl+9u0I1zUgzEBZv5zIOjiCQTwUmHNBm+sGiMZzXy\ndl4CTAmyWb+IPcFM2qzXYMrDUyHOEP0BeooTEpZM4J3zNrKjI57rhuAhAoGAKWDC\nE1K8BdPZCC1RpSAHy8zcrPWIaGiCQx6TPFNPwMU/XTrGi9R7j1oAVTfjsJpYnNV5\nTGNb99XWPV1dPfaH3i7TcczglcjuO/eKsAlqzLUWzkK4IVCKXKgC5D1O2Yk17d03\nt4aYb/Wak0LzaJgJIUD2oYCmSoDBe8K/jX0o+wECgYBnxk9HR/23hjWaxrSnXGDB\nHxLXg9Wz5w0N+gdC/FNxknFOft+nsCMKWMocOtGYhJU3OvkTYYqL1iDsKoMb74xG\nVwB1fuoNrNp+aJ/CzbtZVT1WLzXG41e9cu2TuOy+wpDlryfJAZ6KNVgDOmhh8TR2\nz7T0rt1QSfOZILpiwpR4jg==\n-----END PRIVATE KEY-----\n",
+  "client_email": "noisycontents@thematic-bounty-382700.iam.gserviceaccount.com",
+  "client_id": "107322055541690533468",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/noisycontents%40thematic-bounty-382700.iam.gserviceaccount.com"
+}
+creds = service_account.Credentials.from_service_account_info(info=creds_info, scopes=scope)
+client = gspread.authorize(creds)
+
 async def generate_response(prompt):
     response = openai.Completion.create(
         engine="gpt-3.5-turbo",
@@ -87,26 +103,36 @@ async def lottery(ctx):
     await message.edit(embed=embed)
     
 #------------------------------------------------#
-nltk.download('wordnet')
+sheet2 = client.open('테스트').worksheet('일취월장')
+rows = sheet2.get_all_values()
 
-@bot.command(name='오늘의단어')
-async def word(ctx, language):
-    lang_codes = {'영어': 'eng', '스페인어': 'spa', '중국어': 'cmn-s'}
-    lang = lang_codes.get(language.lower(), None)
-    if lang is None:
-        await ctx.send(f"Sorry, I don't have word lists for the language {language}.")
-        return
+@bot.command()
+async def Authentication(ctx, date):
+    await ctx.message.delete()
+    embed = discord.Embed(title="Authentication", description="Verifying authentication")
+    msg = await ctx.send(embed=embed)
+    await msg.add_reaction("✅")
 
-    synsets = wn.all_synsets(lang=lang)
-    words = random.sample(list(set(synset.lemma_names(lang=lang) for synset in synsets)), 5)
-    embed = discord.Embed(title=f"Word of the day in {language.capitalize()}", color=discord.Color.blue())
-    for word in words:
-        synset = wn.synset(f"{word}.n.{lang}.01")
-        definition = synset.definition()
-        examples = ", ".join(synset.examples()[:3])
-        pronunciation = f"/{synset.pronunciations()[0].pronunciation}/" if synset.pronunciations() else "Not available"
-        embed.add_field(name=word, value=f"Definition: {definition}\nExamples: {examples}\nPronunciation: {pronunciation}", inline=False)
-    await ctx.send(embed=embed)
+    def check(reaction, user):
+        return str(reaction.emoji) == "✅" and discord.utils.get(user.roles, id=922400231549722664) is not None and reaction.message.id == msg.id
+
+    try:
+        reaction, user = await client.wait_for("reaction_add", timeout=60, check=check)
+    except:
+        await msg.edit(embed=discord.Embed(title="Authentication", description="Failed to authenticate"))
+    else:
+        row = [str(user), date]
+        existing_users = sheet.col_values(1)
+        if str(user) in existing_users:
+            index = existing_users.index(str(user)) + 1
+            cell = sheet.find(date, in_row=1)
+            sheet.update_cell(index, cell.col, "✅")
+        else:
+            empty_row = len(existing_users) + 1
+            sheet.insert_row(row, empty_row)
+            cell = sheet.find(date, in_row=1)
+            sheet.update_cell(empty_row, cell.col, "✅")
+        await msg.edit(embed=discord.Embed(title="Authentication", description="Authentication complete"))
 
         
 #Run the bot
