86542987fb911daf22f67ac38dc1e04bbe91f624
piaotaijiong <127836167+piaotaijiong@users.noreply.github.com>
2023-04-06 18:43:53 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -143,7 +143,7 @@ class AuthView(discord.ui.View):
                 col = existing_dates.index(self.date) + 1
                 sheet2.update_cell(index, col, "1")
         await self.message.edit(embed=discord.Embed(title="인증상황", description="인증완료!"), view=self)
-        self.clear_items()  # Remove all items from the view
+        self.stop()
 
 @bot.command(name='인증')
 async def Authentication(ctx, date):
