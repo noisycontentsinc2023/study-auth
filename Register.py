11520b831812f28da60b2f0b86c055d0973bc2e3
piaotaijiong <127836167+piaotaijiong@users.noreply.github.com>
2023-04-12 16:57:09 +0900

    Update and rename Register to Register.py

---
+++ Register.py
@@ -0,0 +1,27 @@
+import discord
+from discord.ext import commands
+
+class Register(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        
+    @commands.command(name='등록')
+    async def Register(ctx):
+        username = str(ctx.message.author)
+    
+        sheet3, rows = await get_sheet3()
+
+        row = 2
+        while (await sheet3.cell(row, 1)).value:
+            row += 1
+
+        await sheet3.update_cell(row, 1, username)
+
+        role = discord.utils.get(ctx.guild.roles, id=1093781563508015105)
+        await ctx.author.add_roles(role)
+
+        embed = discord.Embed(description=f"{ctx.author.mention}님, 랜덤미션스터디에 정상적으로 등록됐습니다!", color=0x00FF00)
+        await ctx.send(embed=embed)
+    
+def setup(bot):
+    bot.add_cog(Register(bot))
