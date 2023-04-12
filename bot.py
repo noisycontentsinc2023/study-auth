c5dd7da73cd6d1396e380f1511e5c8f9cace44a6
piaotaijiong <127836167+piaotaijiong@users.noreply.github.com>
2023-04-12 11:33:47 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -294,8 +294,15 @@ async def mission_count(ctx):
 #------------------------------------------------#
 @bot.command(name="역할")
 async def show_roles(ctx):
-    roles = [role.name for role in ctx.author.roles]
-    embed = discord.Embed(title=f"{ctx.author.name}님의 역할", description=", ".join(roles), color=0x00ff00)
+    roles = ctx.author.roles[1:]  # Exclude the everyone role
+    embed = discord.Embed(title=f"Roles of {ctx.author.name}", color=0x00ff00)
+    
+    # Add each role and its icon to the embed's description
+    for role in roles:
+        embed.description = f"{embed.description}\n{role.mention} {role.name}"
+        if role.icon_url:
+            embed.description = f"{embed.description} {role.icon_url}"
+            
     await ctx.send(embed=embed)
     
 #------------------------------------------------#    
