af2b380ffb3f4155e8d4fdc66566cc9db7376fef
piaotaijiong <127836167+piaotaijiong@users.noreply.github.com>
2023-04-12 14:30:50 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -23,7 +23,7 @@ from discord.ext import tasks, commands
 from discord.ext.commands import Context
 from discord.utils import get
 from urllib.request import Request
-from discord.ui import Select, Button, View, ButtonStyle
+from discord.ui import Select, Button, View
 
 TOKEN = os.environ['TOKEN']
 PREFIX = os.environ['PREFIX']
