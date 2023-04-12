2821166af86c54a063edb4c0915518423c230ac5
piaotaijiong <127836167+piaotaijiong@users.noreply.github.com>
2023-04-12 15:09:40 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -24,6 +24,7 @@ from discord.ext.commands import Context
 from discord.utils import get
 from urllib.request import Request
 from discord.ui import Select, Button, View
+from utils import find_user
 
 TOKEN = os.environ['TOKEN']
 PREFIX = os.environ['PREFIX']
