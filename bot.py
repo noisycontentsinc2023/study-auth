2b716e898904d00dd8ac9eb7135311e681af0c3d
piaotaijiong <127836167+piaotaijiong@users.noreply.github.com>
2023-04-12 18:54:18 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -268,7 +268,7 @@ class AuthButton2(discord.ui.Button):
         self.view.clear_items()
 
         # Send a success message
-        embed = discord.Embed(title='인증완료!', description=f'{self.username}님, 정상적으로 인증되셨습니다')
+        embed = discord.Embed(title='인증완료!', description=f'{ctx.author.mention}님, 정상적으로 인증되셨습니다')
         await interaction.response.edit_message(embed=embed, view=None)
 
 @bot.command(name='누적')
