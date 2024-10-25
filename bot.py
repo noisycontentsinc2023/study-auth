fd6f20d25636814b1e15977d011f552a0f86e638
noisycontentsinc2023 <135790045+noisycontentsinc2023@users.noreply.github.com>
2024-10-25 16:11:21 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -53,7 +53,7 @@ creds_info = {
   "auth_uri": "https://accounts.google.com/o/oauth2/auth",
   "token_uri": "https://oauth2.googleapis.com/token",
   "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/server%40server-439707.iam.gserviceaccount.com"
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/server%40server-439707.iam.gserviceaccount.com",
 }
 
 credentials = Credentials.from_service_account_info(creds_info, scopes=scope)
