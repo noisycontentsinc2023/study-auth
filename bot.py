4fab9a8b0cd3fbac1f411d3b5cc8b13bb0109a6c
noisycontentsinc2023 <135790045+noisycontentsinc2023@users.noreply.github.com>
2024-10-26 23:19:06 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -43,18 +43,21 @@ intents.presences = False
 bot = commands.Bot(command_prefix=PREFIX, intents=intents)
 
 scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
-creds_info = {
+creds_info = 
+{
   "type": "service_account",
-  "project_id": "thematic-bounty-382700",
-  "private_key_id": "502d8dd4f035d15b57bff64ee323d544d28aedc4",
-  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQD4Kze3Hn/yxevG\nzHUklYGSDDs8qKQeyYdd1eWaR0PNKZ2+nwKFGmXGENS6vuy3U81dqI3AVgA3w6UW\nHEaVfPvc31OX5yNCIS0eQxxqWWGJJ5+MbvUC06qXi/7hCup0WK+hoqwjHtUX7AYu\nEDgtf6xd29gSvb3YXs6pvi+2tpwPt0SED6HGPU7qPRsAaPnyUsVCj/mW04ca2iea\nxMsIqxKT6ufNssiXX7qhKyziowneM0lp8BB3k2z+/FGPQOCdi/lIscC9zKbDOIcb\nOZw+B2sd2opp7Dwo3JMIkh3NJevw9hjp0+CFeqemGNsCAiSuFkvydx6BagWaWAPs\nC48nZLNZAgMBAAECggEAF3uUbTMZQZZVoAU5CPYOMY0PfmcJR6IDeX8715BKg8N+\nOhtHBGQJ8Rbm4Ehgcxz+i/AfAK4KnXw5dvkEO1E9Lmph+Tfdg9yKjchlLBGK24z4\nqZPWwpaXl/k+7BnJs2pwbROs5PJeEOJMN+fgPvrrqyJ6RNS4Pf0Dond9AZWwOQLL\naJPFZryK7Bmvtt0H8mDDWdfqmCQTtPJ9PUyDEUeenlyhuek8wH3GHcghOSlsCDF1\nW/3YXM9Vr/arE4V6hTLwXofrUnTuXTfo+DcaOIXpHqIPS+nCyzWZ0kAJ7/uKjhnN\nF4kgv9aXDX9Y7S+3irXazRhowfu2rGuPRO/2+FCuMQKBgQD+JRDctOrKvpl9zDaw\nWT2a3qmYuFf90+b8EkKsWzVBM7neEJlw1ZWxUZzkdHXwkxcM7w93BjZeXJRnI7HZ\n5sHMrRw3p7Cwy0REqC3GSbYMCIZ/98y5Ot5sOXamUCOtYnic1NG2PBzr9h94Nt7d\nZu9D7cD/kaogm9Fl9t1VMD3REQKBgQD5+vvxY0nUkzrPEHfAOnPRqt3fM9ryzmk9\n8WyffmWqaDcvb9pl1F/+/51u00cvh2Q6etvL0J850AB0AKC9QdYzIaSj4LeRNzjA\ns+K6Po5+HAYezxC1cYzFh+slAfX3gX9pa11f3aOltj4h7IXvqBB0iH4rl/i2KQ/G\ntSDa62K9yQKBgAXXEDYiKisSijBr2u3efx3p8/fAdLUug2ZTfRi819Jxv9msg/ol\nzlTOzU4qpvMqTiNL8w0HJYSxl+9u0I1zUgzEBZv5zIOjiCQTwUmHNBm+sGiMZzXy\ndl4CTAmyWb+IPcFM2qzXYMrDUyHOEP0BeooTEpZM4J3zNrKjI57rhuAhAoGAKWDC\nE1K8BdPZCC1RpSAHy8zcrPWIaGiCQx6TPFNPwMU/XTrGi9R7j1oAVTfjsJpYnNV5\nTGNb99XWPV1dPfaH3i7TcczglcjuO/eKsAlqzLUWzkK4IVCKXKgC5D1O2Yk17d03\nt4aYb/Wak0LzaJgJIUD2oYCmSoDBe8K/jX0o+wECgYBnxk9HR/23hjWaxrSnXGDB\nHxLXg9Wz5w0N+gdC/FNxknFOft+nsCMKWMocOtGYhJU3OvkTYYqL1iDsKoMb74xG\nVwB1fuoNrNp+aJ/CzbtZVT1WLzXG41e9cu2TuOy+wpDlryfJAZ6KNVgDOmhh8TR2\nz7T0rt1QSfOZILpiwpR4jg==\n-----END PRIVATE KEY-----\n",
-  "client_email": "noisycontents@thematic-bounty-382700.iam.gserviceaccount.com",
-  "client_id": "107322055541690533468",
+  "project_id": "vibrant-airship-439708-t4",
+  "private_key_id": "2dec0b1ad9a10653c418f095b9d64d9c37487253",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQCzvdF+G0DAx2pn\n7UG75pH9ihhkZ3PIbd48y6IQeLh/P/jxm6FCUHaRm4Kf23VMsBk1gHvG0/X96yHI\nLNg4PDxctcLyLYLcUG2SRuGQ9QOxSDDf08cFVTwh0gMof9rjKy6Br0LQNiTbNRH7\nqb5WraU3kfVJTsyjeADAejsm7vpzuGcln61FX18V2M7zHlth5bIDHH7EkSyp0h5J\nCipslAa8rzvLOsVuQ3waL07APou7441kqD8Yc6DGt7PyO0DecCJ6VM169R1XJAzt\nRF4Ib7ixC+ACaRQ5fC8aEmfJZEzuvQ4hzFrDBTfFte9BnWePAVaSki8kaV7cwQP4\n+Z1w271ZAgMBAAECggEAMJHrIc4gfdeFR+cje7CPI7mk5MDV10PlKX2HYjcU+AHj\n9fntphtTJv6kWik19c6iSURabjyJvR9ERjDkDrIsR4swTjwBICK1sTDpNEAZStcs\nqdcy8Rx6vtLe2ptOHVPmYdHn1pbC999L6DsSvZ53xl/xLemWv9emMFz3TgqG8Czg\nl+n/5E1prUsBPB19pcLges06UdVJiz8XDvUoLMKka2Mszjm6fBh0+TQddcf0Js2N\nkxMNCSJTD7FFkZlnwIoST/E7EMeN/0iUl5fzUC1fZ3RDsKpaqrr0CZim7eqSoTPT\nlLDu/fOFR4f/G63M51lE4du7rg51/cSCxbgClH73vwKBgQDof0AmTFep7HMX3zI5\nd2Orjb3DoK3Jh1DwR5OUI0zsSXNacDiUT53EWCfP/uuLvda+Ohh9Ot4dloc9Qc+K\n4ZIiwDc/EyT506/q2qPHmmUOWt8fyGjRpzB9PlbT/bgmyScbGH/XahSOT5nMpC05\nAEOqL2O2dwY1+V6epza34LKDTwKBgQDF6VC4adu2bHB77mvognE5dHiGbzqVr2wb\nPAqF3bAevFsUafDMgS8o7YgpTYlpWQQB8lecR0519Dc9T1DxANLE5mvGZD9+KL6A\nVa24yBM+WWODdrw4aHPTnlSGCG+qnPtSYnmKFABkuYE4e9DlM24dNf3t4OISPYdo\nlxmwyTOq1wKBgHDv116DORY+2I2hCwmlOBJOkP6lX6YEiEmfy01rdGNXLqSg3KsW\nnaGR3Q0JXKwN9HGFUP4MCXc/9sPH4yQofM9KpcuK5ixBKzQfLu23zx0RAU+0D07A\nMyqzjIP/vGOC1O8xkHA4hHG4sxIYMit9OGFEEeDBoo+3acIRod/iU3S7AoGAHwLB\nCDRmHf593Z1tQSB3V1ZdsErrdhrnqJjO2Uz0VMyUxO0YsyKm3hXiGP6yQlSVYSy0\n96Lgs3DR2wR7nVfQrtOgHZUFseH2f6ttLiADi+h7xFSth3UueQE9I0ddH/G7G5yc\niLs7mIzMp6oXAGL/pQoZwNVU9yC2//d1KhNMyj8CgYAQt1cHNxWm9GX9JMhRpYey\nP/IvmYhYKNzLbr00c2chUquAPFuwLGFOhbvLI7VXTEpQO9lWVa9zclBNKxznKPV2\np17uQGKcTCVWYPzcCFxTDVgSMMEN8PE8X+ZU8E9a9R4WRvq35Qk3jj55oYmrYnM8\nlq8cwYeLxN80i/6DYnt8Kw==\n-----END PRIVATE KEY-----\n",
+  "client_email": "server@vibrant-airship-439708-t4.iam.gserviceaccount.com",
+  "client_id": "105018887118886094462",
   "auth_uri": "https://accounts.google.com/o/oauth2/auth",
   "token_uri": "https://oauth2.googleapis.com/token",
   "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/noisycontents%40thematic-bounty-382700.iam.gserviceaccount.com"
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/server%40vibrant-airship-439708-t4.iam.gserviceaccount.com",
+  "universe_domain": "googleapis.com"
 }
+
 
 credentials = Credentials.from_service_account_info(creds_info, scopes=scope)
 aio_creds = credentials
